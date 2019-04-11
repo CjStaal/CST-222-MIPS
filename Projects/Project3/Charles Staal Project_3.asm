5f132617d88d4402d6ccea6e5a08101c74447bf7
Charles Joseph Staal <Charles.Staal@gmail.com>
2019-04-11 01:42:58 -0400

    Just added some comments
    I believe all lines need a comment in assembler.

--- Projects/Project3/Charles Staal Project_3.asm
+++ Projects/Project3/Charles Staal Project_3.asm
@@ -50,14 +50,20 @@
 ##############################
 
 set_map_default:
+	# There are no arguments. This function merely resets all icons to their default state
 
-	la $t0, STARTING_ADDRESS
+	# t0 = starting address
+	# t1 = default cell colors
+	# t2 = default cell icon
+	# t3 = counter
+
+	la $t0, STARTING_ADDRESS			# The starting address of the cells
 	li $t1, DEFAULT_CELL_COLOR			# t1 will be used for color
 	li $t2, DEFAULT_CELL_ICON			# t2 will be used for icon
-	li $t3, 0
+	li $t3, 0					# Counter will start at zero and go until it reaches 200
 
 	map_default_loop:
-		beq $t3, 200, default_map_done		# There are two hundred bytes in the map
+		beq $t3, 200, default_map_done		# There are two hundred bytes in the map and we must go through them all
 		sb $t1, 0($t0)				# The first byte stores the color
 		sb $t2, 1($t1)				# The second byte stores the icon
 		addi $t0, $t0, 2			# We must increment by two since we are modifying two bytes each
@@ -72,9 +78,9 @@ smiley:
 	# t1 = Color of cell background (high bits) and foreground (low bits)
 	# t2 = icon of cell
 
-	pack_stack()
+	pack_stack()					# We are calling a function inside this function, so we need to pack the stack
 
-	jal set_map_default
+	jal set_map_default				# Will set all cells to their default state
 
 	li $t0, STARTING_ADDRESS			# t0 will be starting address
 
@@ -120,8 +126,8 @@ smiley:
 	sb $t1, 171($t0)				# The byte right after is the icon for said cell
 	# finished setting mouth
 
-	unpack_stack()
-	jr $ra
+	unpack_stack()					# The stack must be unpacked before returning
+	jr $ra						# returns to previous address
 
 ##############################
 # PART 2 FUNCTIONS
