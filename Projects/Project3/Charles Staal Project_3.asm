03e2008349da463088a9baf926e32e390052ced0
Charles Joseph Staal <Charles.Staal@gmail.com>
2019-04-11 15:21:08 -0400

    added macro

--- Projects/Project3/Charles Staal Project_3.asm
+++ Projects/Project3/Charles Staal Project_3.asm
@@ -3,6 +3,7 @@
 # name: Charles Staal
 # scccid: 01040168
 ##############################################################
+
 .text
 
 # Colors
@@ -40,19 +41,19 @@
 .eqv WHITE_BACKGROUND 240
 
 # Icons
-.eqv ZERO 48
-.eqv ONE 49
-.eqv TWO 50
-.eqv THREE 51
-.eqv FOUR 52
-.eqv FIVE 53
-.eqv SIX 54
-.eqv SEVEN 55
-.eqv EIGHT 56
-.eqv BOMB 66
-.eqv EXPLOSION 69
-.eqv FLAG 70
-.eqv NULL 0
+.eqv ZERO_ICON 48
+.eqv ONE_ICON 49
+.eqv TWO_ICON 50
+.eqv THREE_ICON 51
+.eqv FOUR_ICON 52
+.eqv FIVE_ICON 53
+.eqv SIX_ICON 54
+.eqv SEVEN_ICON 55
+.eqv EIGHT_ICON 56
+.eqv BOMB_ICON 66
+.eqv EXPLOSION_ICON 69
+.eqv FLAG_ICON 70
+.eqv NULL_ICON 0
 
 # Starting address for map
 .eqv STARTING_ADDRESS 4294901760
@@ -78,6 +79,45 @@
 .eqv CLOSE_FILE 16
 
 ##############################
+# PART 0 MACROS
+##############################
+
+# Going by http://www.cs.ucsb.edu/~franklin/64/resources/spim/BookCallConvention.htm
+.macro push_all_stack()
+	addi $sp $sp, -64				# -(L+R+A+1+1)*4=64 A = maximum # of args (4), R = Preserved Registers (8), L = words of local data (1), FP (1), SP (1), + padding
+	sw $a0, 0($sp)
+	sw $a1, 4($sp)
+	sw $a2, 8($sp)
+	sw $a3, 12($sp)
+	sw $s0, 20($sp)
+	sw $s1, 24($sp)
+	sw $s2, 28($sp)
+	sw $s3, 32($sp)
+	sw $s4, 36($sp)
+	sw $s5, 40($sp)
+	sw $s6, 44($sp)
+	sw $s7, 48($sp)
+	sw $fp, 54($sp)
+	sw $ra, 56($sp)
+
+.macro pop_all_stack()
+	lw $ra, 0($sp)
+	lw $fp, 4($sp)
+	lw $s7, 8($sp)
+	lw $s6, 12($sp)
+	lw $s5, 16($sp)
+	lw $s4, 20($sp)
+	lw $s3, 24($sp)
+	lw $s2, 28($sp)
+	lw $s1, 32($sp)
+	lw $s0, 36($sp)
+	lw $a3, 44($sp)
+	lw $a2, 48($sp)
+	lw $a1, 52($sp)
+	lw $a0, 56($sp)
+	addi $sp, $sp, 64
+
+##############################
 # PART 1 FUNCTIONS
 ##############################
 .globl main
@@ -92,26 +132,28 @@ smiley:
 	# t0 = Starting address
 	# t1 = icon of cell
 	# t2 = Color of cell background (high bits) and foreground (low bits)
+	# t3 = Counter set at MAX_CELLS and will decrement
 
+	# We are setting all cells to default values
 	li $t0, STARTING_ADDRESS			# The starting address of the cells
 	li $t1, DEFAULT_CELL_ICON			# t1 will be used for icon
 	li $t2, DEFAULT_CELL_COLOR			# t2 will be used for color
 	li $t3, MAX_CELLS				# Counter will start at MAX_CELLS and go until it reaches 0
 
 	map_default_loop:
-		beqz $t3, MAX_CELLS, default_map_done	# There are two hundred bytes in the map and we must go through them all
+		beqz $t3, default_map_done	# There are two hundred bytes in the map and we must go through them all
 		sb $t1, 0($t0)				# The first byte stores the icon
 		sb $t2, 1($t0)				# The second byte stores the color
 		addi $t0, $t0, 2			# We must increment by two since we are modifying two bytes each
 		addi $t3, $t3, -1			# Same as above
 		b map_default_loop
-
 	default_map_done:
+	# We are done setting all cells to default values
 
 	li $t0, STARTING_ADDRESS			# t0 will be starting address
 	
 	# We are setting the eyes
-	li $t1, BOMB					# The eyes will have a bomb icon
+	li $t1, BOMB_ICON				# The eyes will have a bomb icon
 	li $t2, YELLOW_BACKGROUND			# The eyes will have a yellow background color
 	addi $t2, $t2, GRAY_FOREGROUND			# The eyes will have gray as foreground color
 
@@ -129,7 +171,7 @@ smiley:
 	# Finished setting eyes
 
 	# We are setting the mouth
-	li $t1, EXPLOSION				# The smile will have an explosion icon
+	li $t1, EXPLOSION_ICON				# The smile will have an explosion icon
 	li $t2, RED_BACKGROUND				# The smile will have a red background
 	addi $t2, $t2, WHITE_FOREGROUND			# The smile will have a white foreground
 
@@ -178,8 +220,8 @@ load_map:
 	# There are no arguments for this functino
 	# s0 = Base address of cell_array
 	# s1 = cell location/offset for cell array
-
-	la $s0, cell_array				# s0 will be the base address of the cell array
+	# s2 = counter set at MAX_CELLS and will decrement
+	la $s0, Cell_Array				# s0 will be the base address of the cell array
 	li $s1, 0					# s1 will be the cell location/offset of the cell array
 
 	jr $ra
@@ -221,6 +263,6 @@ search_cells:
 
 .data
 .align 2
-cursor_row: .word -1
-cursor_col: .word -1
-cells_array: .byte 100
+Cursor_Row: .word -1
+Cursor_Col: .word -1
+Cell_Array: .byte 100
