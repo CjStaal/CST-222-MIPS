6223f35cf22ef391b5288ce143a7b797df035cdd
Charles Joseph Staal <Charles.Staal@gmail.com>
2019-04-11 01:38:50 -0400

    Added eqv's, finished smiley function
    added color, icon, starting address, and default cell info eqv's.
    
    created and finished smiley function.
    
    created a helper function called set_map_default and finished it.
    
    finished file functions.

--- Projects/Project3/Charles Staal Project_3.asm
+++ Projects/Project3/Charles Staal Project_3.asm
@@ -24,16 +24,103 @@
 .eqv WHITE 0x1111
 
 # Icons
+.eqv ZERO '0'
+.eqv ONE '1'
+.eqv TWO '2'
+.eqv THREE '3'
+.eqv FOUR '4'
+.eqv FIVE '5'
+.eqv SIX '6'
+.eqv SEVEN '7'
+.eqv EIGHT '8'
 .eqv BOMB 'B'
 .eqv EXPLOSION 'E'
 .eqv FLAG 'F'
 .eqv NULL '\0'
 
+# Starting address for map
+.eqv STARTING_ADDRESS 0xffff0000
+
+# Default cell-state
+.eqv DEFAULT_CELL_COLOR 0x00001111
+.eqv DEFAULT_CELL_ICON '\0'
+
 ##############################
 # PART 1 FUNCTIONS
 ##############################
 
+set_map_default:
+
+	la $t0, STARTING_ADDRESS
+	li $t1, DEFAULT_CELL_COLOR			# t1 will be used for color
+	li $t2, DEFAULT_CELL_ICON			# t2 will be used for icon
+	li $t3, 0
+
+	map_default_loop:
+		beq $t3, 200, default_map_done		# There are two hundred bytes in the map
+		sb $t1, 0($t0)				# The first byte stores the color
+		sb $t2, 1($t1)				# The second byte stores the icon
+		addi $t0, $t0, 2			# We must increment by two since we are modifying two bytes each
+		addi $t3, $t3, 2			# Same as above
+		b map_default_loop
+
+	default_map_done:
+	jr $ra
+
 smiley:
+	# t0 = Starting address
+	# t1 = Color of cell background (high bits) and foreground (low bits)
+	# t2 = icon of cell
+
+	pack_stack()
+
+	jal set_map_default
+
+	li $t0, STARTING_ADDRESS			# t0 will be starting address
+
+	# We are setting the eyes
+	lui $t1, YELLOW					# The eyes will have a yellow background color
+	addi $t1, $t1, GRAY				# The eyes will have gray as foreground color
+	li $t2, BOMB					# The eyes will have a bomb icon
+
+	sb $t0, 46($t0)					# First coord is ((2*20)+(3*2)) + starting address	
+	sb $t1, 47($t0)					# The byte right after is the icon for said cell
+
+	sb $t0, 52($t0)					# First coord is ((2*20)+(6*2)) + starting address	
+	sb $t1, 53($t0)					# The byte right after is the icon for said cell
+
+	sb $t0, 66($t0)					# First coord is ((3*20)+(3*2)) + starting address	
+	sb $t1, 67($t0)					# The byte right after is the icon for said cell
+
+	sb $t0, 72($t0)					# First coord is ((3*20)+(6*2)) + starting address	
+	sb $t1, 73($t0)					# The byte right after is the icon for said cell
+	# Finished setting eyes
+
+	# We are setting the mouth
+	lui $t1, RED					# The smile will have a red background
+	addi $t1, $t1, WHITE				# The smile will have a white foreground
+	li $t2, EXPLOSION				# The smile will have an explosion icon
+
+	sb $t0, 124($t0)				# First coord is ((6*20)+(2*2)) + starting address	
+	sb $t1, 125($t0)				# The byte right after is the icon for said cell
+
+	sb $t0, 134($t0)				# First coord is ((6*20)+(7*2)) + starting address	
+	sb $t1, 135($t0)				# The byte right after is the icon for said cell
+
+	sb $t0, 146($t0)				# First coord is ((7*20)+(3*2)) + starting address	
+	sb $t1, 147($t0)				# The byte right after is the icon for said cell
+
+	sb $t0, 152($t0)				# First coord is ((7*20)+(6*2)) + starting address	
+	sb $t1, 153($t0)				# The byte right after is the icon for said cell
+
+	sb $t0, 168($t0)				# First coord is ((8*20)+(4*2)) + starting address	
+	sb $t1, 169($t0)				# The byte right after is the icon for said cell
+
+	sb $t0, 170($t0)				# First coord is ((8*20)+(5*2)) + starting address	
+	sb $t1, 171($t0)				# The byte right after is the icon for said cell
+	# finished setting mouth
+
+	unpack_stack()
 	jr $ra
 
 ##############################
@@ -41,9 +128,18 @@ smiley:
 ##############################
 
 open_file:
+	# a0 = filename
+	li $a1, 0
+	li $a2, 0
+	li $v0, 13
+	syscall
+
 	jr $ra
 
 close_file:
+	li $v0, 16
+	syscall
+
 	jr $ra
 
 load_map:
